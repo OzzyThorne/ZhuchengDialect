0bd3f626999da4c528c0742b8cc63d21a13a4a61
孙骄 <xzzzf.dsx@gmail.com>
2019-01-04 10:42:18 +0800

    数字部分。

--- app/src/main/java/com/ozzy/android/zhucheng/NumbersActivity.java
+++ app/src/main/java/com/ozzy/android/zhucheng/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.ozzy.android.zhucheng;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,18 +16,25 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+        ArrayList<String> numbers = new ArrayList<String>();
+        numbers.add("一");
+        numbers.add("二");
+        numbers.add("三");
+        numbers.add("四");
+        numbers.add("五");
+        numbers.add("六");
+        numbers.add("七");
+        numbers.add("八");
+        numbers.add("九");
+        numbers.add("十");
 
-    String[] chineseNumbers = new String[10];
-    chineseNumbers[0] = "一";
-    chineseNumbers[1] = "二";
-    chineseNumbers[2] = "三";
-    chineseNumbers[3] = "四";
-    chineseNumbers[4] = "五";
-    chineseNumbers[5] = "六";
-    chineseNumbers[6] = "七";
-    chineseNumbers[7] = "八";
-    chineseNumbers[8] = "九";
-    chineseNumbers[9] = "十";
-
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        ArrayList<TextView> numberView = new ArrayList<TextView>();
+        for (int i = 0 ; i < numbers.size() ; i++){
+            TextView perNumber = new TextView(this);
+            perNumber.setText(numbers.get(i));
+            numberView.add(perNumber);
+            rootView.addView(numberView.get(i));
+        }
     }
 }
